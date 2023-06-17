7b16d8b6ff8c0397b4fb54d40adc98f109d1ad21
farahmango <farahmangoo@gmail.com>
2023-06-17 10:35:40 +0300

    add

---
+++ src/Main.java
@@ -0,0 +1,268 @@
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.github.dockerjava.core.dockerfile.DockerfileStatement.Add;
+
+public class Main extends Parameters{
+
+	@BeforeTest
+	public void beforeTest() {
+		driver.get("https://www.demoblaze.com");
+		driver.manage().window().maximize();
+	}
+	
+	
+	@Test()
+	public void A_signUp() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement signUp = driver.findElement(By.xpath("/html/body/nav/div[1]/ul/li[8]/a"));
+		signUp.click();
+	
+	// Store the parent window handle
+	String parentWindow = driver.getWindowHandle();
+
+	// Perform an action that triggers the modal window
+
+	// Get all window handles
+	Set<String> windowHandles = driver.getWindowHandles();
+
+	// Switch to the modal window
+	for (String windowHandle : windowHandles) {
+	    if (!windowHandle.equals(parentWindow)) {
+	        driver.switchTo().window(windowHandle);
+	        break;
+	    }
+	}
+
+	// Now you are working on the modal window
+	// Perform actions on the modal window
+	WebElement userNamee = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[1]/input"));
+	userNamee.sendKeys(userName);
+	Thread.sleep(5000);
+	WebElement pass = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[2]/input"));
+	pass.sendKeys(password);
+	Thread.sleep(5000);
+	WebElement submit =driver.findElement(By.xpath("/html/body/div[2]/div/div/div[3]/button[2]"));
+	submit.click();
+	Thread.sleep(5000);
+
+    Alert a = driver.switchTo().alert();
+  //accept alert
+    a.accept();
+    driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/button")).click();
+	// Switch back to the parent window
+	driver.switchTo().window(parentWindow);
+	
+	
+		}	
+	
+	
+	
+	@Test()
+	public void B_signIn() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement signUp = driver.findElement(By.xpath("/html/body/nav/div[1]/ul/li[5]/a"));
+		signUp.click();
+	
+//	// Store the parent window handle
+//	String parentWindow = driver.getWindowHandle();
+//
+//	// Perform an action that triggers the modal window
+//
+//	// Get all window handles
+//	Set<String> windowHandles = driver.getWindowHandles();
+//
+//	// Switch to the modal window
+//	for (String windowHandle : windowHandles) {
+//	    if (!windowHandle.equals(parentWindow)) {
+//	        driver.switchTo().window(windowHandle);
+//	        break;
+//	    }
+//	}
+
+	// Now you are working on the modal window
+	// Perform actions on the modal window
+	WebElement userNamee = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[1]/input"));
+	userNamee.sendKeys(userName);
+	Thread.sleep(5000);
+	WebElement pass = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[2]/input"));
+	pass.sendKeys(password);
+	Thread.sleep(5000);
+	WebElement submit =driver.findElement(By.xpath("/html/body/div[3]/div/div/div[3]/button[2]"));
+	submit.click();
+	Thread.sleep(5000);
+
+	// Switch back to the parent window
+//	driver.switchTo().window(parentWindow);
+	
+	
+		}	
+	
+	
+	
+	@Test()
+	public void C_selectSamsungRandomly() throws InterruptedException {
+		List<WebElement> selectTab = driver.findElements(By.className("hrefch"));
+		Thread.sleep(5000);
+		List<WebElement> array = new ArrayList<>();
+		
+		//get samsung phones only and push them into the array 
+		for(int i = 0 ; i< selectTab.size(); i++) {
+			String text = selectTab.get(i).getText();
+
+			if(text.contains("Samsung")) {			
+				WebElement samsung = selectTab.get(i);
+				 array.add(samsung);	
+				}	
+			}
+		//add sumsung phone to the cart 
+		for(int i = 0 ; i< array.size(); i++) {
+           array.get(i).click();
+            
+			Thread.sleep(3000);
+			WebElement addToCart = driver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div[2]/div/a"));
+			addToCart.click();
+			Thread.sleep(3000);
+
+		    Alert a = driver.switchTo().alert();
+		  //accept alert
+		    a.accept();
+			Thread.sleep(5000);
+			driver.navigate().back();
+			driver.navigate().back();
+			
+        }	
+	}	
+	
+	@Test()
+	public void D_selectPhonesRandomly() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement phoneTab = driver.findElement(By.xpath("/html/body/div[5]/div/div[1]/div/a[2]"));
+		phoneTab.click();
+		Thread.sleep(3000);
+
+		List<WebElement> selectTab = driver.findElements(By.className("hrefch"));
+
+		for(int i = 0 ; i< 3; i++) {
+			int random = rand.nextInt(selectTab.size());
+			selectTab.get(random).click();
+			
+			Thread.sleep(3000);
+			WebElement addToCart = driver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div[2]/div/a"));
+			addToCart.click();
+			Thread.sleep(3000);
+
+		    Alert a = driver.switchTo().alert();
+		  //accept alert
+		    a.accept();
+			Thread.sleep(5000);
+			driver.navigate().back();
+			driver.navigate().back();
+			
+		}
+	}
+	
+	@Test()
+	public void E_selectLaptopsRandomly() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement laptopTab = driver.findElement(By.xpath("/html/body/div[5]/div/div[1]/div/a[3]"));
+		laptopTab.click();
+		Thread.sleep(3000);
+
+		List<WebElement> selectTab = driver.findElements(By.className("hrefch"));
+
+		for(int i = 0 ; i< 2; i++) {
+			int random = rand.nextInt(selectTab.size());
+			selectTab.get(random).click();
+			
+			Thread.sleep(3000);
+			WebElement addToCart = driver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div[2]/div/a"));
+			addToCart.click();
+			Thread.sleep(3000);
+
+		    Alert a = driver.switchTo().alert();
+		  //accept alert
+		    a.accept();
+			Thread.sleep(5000);
+			driver.navigate().back();
+			driver.navigate().back();
+
+			
+		}		
+	}
+	
+	
+	@Test()
+	public void F_selectMonitorsandomly() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement monitorsTab = driver.findElement(By.xpath("/html/body/div[5]/div/div[1]/div/a[4]"));
+		monitorsTab.click();
+		Thread.sleep(3000);
+
+		List<WebElement> selectTab = driver.findElements(By.className("hrefch"));
+			int random = rand.nextInt(selectTab.size());
+			selectTab.get(random).click();
+			
+			Thread.sleep(3000);
+			WebElement addToCart = driver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div[2]/div/a"));
+			addToCart.click();
+			Thread.sleep(3000);
+
+		    Alert a = driver.switchTo().alert();
+		  //accept alert
+		    a.accept();
+			Thread.sleep(5000);
+			driver.navigate().back();
+			driver.navigate().back();	
+	
+	}
+	
+	@Test()
+	public void G_checkOutProcess() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.navigate().to("https://www.demoblaze.com/cart.html");
+		
+		WebElement placeOrder = driver.findElement(By.xpath("/html/body/div[6]/div/div[2]/button"));
+		placeOrder.click();
+		
+		WebElement name = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[1]/input"));
+		name.sendKeys(Name);
+		WebElement country = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[2]/input"));
+		country.sendKeys(Country);
+		WebElement city = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[3]/input"));
+		city.sendKeys(City);
+		WebElement creditCard = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[4]/input"));
+		creditCard.sendKeys(CreditCard);
+		WebElement month = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[5]/input"));
+		month.sendKeys(Month);
+		WebElement year = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div[6]/input"));
+		year.sendKeys(Year);
+
+		WebElement submit = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[3]/button[2]"));
+		submit.click();
+		
+		Thread.sleep(3000);
+		WebElement okButton =  driver.findElement(By.xpath("/html/body/div[10]/div[7]/div/button"));
+		okButton.click();
+	}
+	
+	
+	
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+		
+		
+	}
+	
+}
---
+++ src/Parameters.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Parameters {
+  
+	WebDriver driver = new ChromeDriver();
+	static String userName = "farah";
+	static String password ="farah";
+	
+	Random rand = new Random();
+	
+	static String Name = "farahT";
+	static String Country ="jordan";
+	static String City ="amman";
+	static String CreditCard ="visa";
+	static String Month ="june";
+	static String Year ="2023";
+
+	
+
+}
